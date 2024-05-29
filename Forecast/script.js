177c0deed196ec9eaa599346608164cfaa8db371
OlenaKolyada <163011315+OlenaKolyada@users.noreply.github.com>
2024-05-29 18:22:20 +0200

    Add files via upload

--- Forecast/script.js
+++ Forecast/script.js
@@ -1,60 +1,99 @@
 'use strict'
 
-const btnGetWeather = document.getElementById('get__weather');
-const divTable = document.getElementById('div__table');
-const cityDropdown = document.getElementById('city');
-let data;
-const arrHeaders = ['Day', 'Picture', 'Weather', 'T min', 'T max'];
-let weatherTable;
-let cityTitle;
-let dateTitle;
+const btnWeather = document.getElementById('weather');
+const btnGraphics = document.getElementById('graphics');
+const btnClear = document.getElementById('clear');
+const divWeather = document.getElementById('div__weather');
+const header = document.getElementById('header');
+const divGraphics = document.getElementById('div__graphics');
+const inputField = document.getElementById('city');
+const arrHeaders = ['Day', 'Weather', 'T min', 'T max'];
+let data = null;
+let weatherTable,
+    cityTitle,
+    errorTitle,
+    graphicTable;
+let arrDays = [];
 
-// Button Get Weather
-btnGetWeather.addEventListener('click', function() {
-    const selectedCity = cityDropdown.options[cityDropdown.selectedIndex].value;
-    clearTable();
-    fetchWeather(selectedCity);
+// Default State
+btnWeather.disabled = true;
+btnGraphics.disabled = true;
+btnClear.disabled = true;
+
+let inputCity = document.getElementById('city').value;
+
+inputField.addEventListener('input', function() {
+    if (inputField.value) {
+        btnWeather.disabled = false;
+    } 
 });
 
-function fetchWeather (city) {
+// Button Weather
+btnWeather.addEventListener('click', function() {
+    inputCity = document.getElementById('city').value;
+    clearTable();
+    fetchWeather(inputCity, inputField);
+    btnGraphics.disabled = false;
+    btnClear.disabled = false;
+    
+});
 
-    fetch(`${city}.json`)
-    .then(response => response.json())
+// Key Enter
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+        event.preventDefault();
+        btnWeather.click();
+    }
+});
+
+// Button Graphics
+btnGraphics.addEventListener('click', function() {
+        buildGraphic();
+
+})
+
+function fetchWeather (city, inputElement) {
+
+    fetch(`https://www.prevision-meteo.ch/services/json/${city}`)
+    // fetch('paris.json')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
     .then(fetchedData => {
         data = fetchedData;
         
         if(data) {
-            if(!cityTitle && !dateTitle && !weatherTable) {
-                
+            if(!cityTitle && !weatherTable) {
+                inputElement.value = '';
                 // Creation City Title
-                cityTitle = document.createElement('h1');
-                cityTitle.textContent = data.city_info.name;
-                divTable.appendChild(cityTitle);
+                cityTitle = document.createElement('h3');
+                cityTitle.textContent = data.city_info.name + ', ' + data.current_condition.date;
+                header.appendChild(cityTitle);
                 
-                // Creation Date Title
-                dateTitle = document.createElement('h2');
-                dateTitle.textContent = data.current_condition.date;
-                divTable.appendChild(dateTitle);
 
                 // Creation Table
                 weatherTable = document.createElement('table');
+                weatherTable.classList.add('weather');
                 let weatherTableHead = document.createElement('thead');
                 let weatherTableBody = document.createElement('tbody');
 
                 weatherTable.appendChild(weatherTableHead);
                 weatherTable.appendChild(weatherTableBody);
-                divTable.appendChild(weatherTable);
+                divWeather.appendChild(weatherTable);
 
                 // Creation Table Headers
                 let headerRow = weatherTableHead.insertRow();
                 arrHeaders.forEach(element => {
-                    let headerCell = document.createElement('th');
-                    headerCell.textContent = element;
-                    headerRow.appendChild(headerCell);
+                let headerCell = document.createElement('th');
+                headerCell.textContent = element;
+                headerRow.appendChild(headerCell);
                 });
                 
                 // Creating Days Array
-                let arrDays = [];
+                
                 Object.values(data).forEach(element => {
                     if (element.hasOwnProperty("day_short")) {
                     arrDays.push(element);
@@ -67,8 +106,7 @@ function fetchWeather (city) {
                     
                     const cellValues = [
                         element.day_long,
-                        `<img src="${element.icon}" />`,
-                        element.condition,
+                        element.condition + `<img src="${element.icon}" />`,
                         element.tmin + '℃',
                         element.tmax + '℃'
                     ];
@@ -80,20 +118,124 @@ function fetchWeather (city) {
                 });
             };
         };
-    })
-    .catch(error => console.error('Error loading JSON:', error));
 
+        
+    
+    })
+    .catch(error => {
+        console.error('Error loading JSON:', error.message);
+        errorTitle = document.createElement('h3');
+        errorTitle.classList.add('alert');
+        errorTitle.textContent = 'Not found';
+        header.appendChild(errorTitle);
+        inputElement.value = '';
+        btnGraphics.disabled = true;
+    });
 }
 
+// Button Clear
+btnClear.addEventListener('click', function() {
+    clearTable();
+})
+
 function clearTable() {
-    if (cityTitle || dateTitle || weatherTable) {
-        weatherTable.remove();
-        weatherTable = null;
-       
-        cityTitle.remove();
-        cityTitle = null;
-        
-        dateTitle.remove();
-        dateTitle = null;
+    btnWeather.disabled = true;
+    btnGraphics.disabled = true;
+
+    if (graphicTable) {
+        graphicTable.remove();
+        graphicTable = null;
     }
+
+    if (cityTitle || weatherTable || errorTitle) {
+        if (cityTitle) {
+            cityTitle.remove();
+            cityTitle = null;
+        }
+        if (weatherTable) {
+            weatherTable.remove();
+            weatherTable = null;
+        }
+        if (errorTitle) {
+            errorTitle.remove();
+            errorTitle = null;
+        }
+    }
+
+    arrDays = [];
+    btnClear.disabled = true;
+
 };
+
+
+function buildGraphic() {
+    if(!graphicTable) {
+
+
+    // Creation Graphic Table
+    graphicTable = document.createElement('table');
+    graphicTable.classList.add('graphics');
+    let graphicTableHead = document.createElement('thead');
+    let graphicTableBody = document.createElement('tbody');
+
+    graphicTable.appendChild(graphicTableHead);
+    graphicTable.appendChild(graphicTableBody);
+    divGraphics.appendChild(graphicTable);
+    
+
+let k = 30;
+
+for (let i = 0; i < 4; i++) {
+    const graphicRow = graphicTable.insertRow();
+    
+    for (let j = 0; j < 6; j++) {
+        const cell = graphicRow.insertCell();
+        if (j === 0) {
+           cell.textContent = k + '℃';
+            k -= 10;
+        } else if (i === 3 && j > 0) {
+            const index = j - 1;
+            if (index < arrDays.length) {
+                const day = arrDays[index];
+
+                const textMin = document.createElement('div');
+                textMin.classList.add('text__min');
+                textMin.textContent = day.tmin + '℃';
+                cell.appendChild(textMin);
+
+                const rectangleMin = document.createElement('div');
+                rectangleMin.classList.add('rectangle__min');
+                rectangleMin.style.height = day.tmin * 7.9 + 'px';
+                cell.appendChild(rectangleMin);
+                
+
+                
+                const rectangleMax = document.createElement('div');
+                rectangleMax.classList.add('rectangle__max');
+                rectangleMax.style.height = day.tmax * 7.9 + 'px';
+                cell.appendChild(rectangleMax);
+            }
+        }
+    }
+}
+
+
+
+    const lastRow = graphicTable.insertRow();
+    
+    
+    for (let i = 0; i < 6; i++) {
+        const lastRowCell = lastRow.insertCell();
+        if (i === 0) {
+            continue;
+        }
+        if (i === 5) {
+            lastRowCell.innerHTML = arrDays[arrDays.length - 1].day_short;
+            break;
+        }
+        lastRowCell.innerHTML = arrDays[i - 1].day_short;
+    }
+
+    btnGraphics.disabled = true;
+    }
+}
